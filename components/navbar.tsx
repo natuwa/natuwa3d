cf5405186365be043de03cd1a9d86a0d4500cf01
natuwa <71311324+natuwa@users.noreply.github.com>
2026-04-11 15:12:32 +0530

    Update navbar.tsx
    mobile button update 4

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -83,8 +83,10 @@ export function Navbar() {
               >
                 Contact
               </Link>
-              <a href="https://docs.google.com/forms/d/e/1FAIpQLSeBD0w5-UBiK0ZF5UGAA-RrI41OaGaBodq67aEMiVJNUcPMLw/viewform" target="_blank"
-  rel="noopener noreferrer" className="block bg-primary text-primary-foreground px-4 py-2 rounded-md">
+              <a href="https://docs.google.com/forms/d/e/1FAIpQLSeBD0w5-UBiK0ZF5UGAA-RrI41OaGaBodq67aEMiVJNUcPMLw/viewform"
+                 target="_blank" rel="noopener noreferrer"
+                 onClick={() => setIsOpen(false)}
+                 className="block bg-primary text-primary-foreground px-4 py-2 rounded-md">
                 Book Now
               </a>
             </div>
