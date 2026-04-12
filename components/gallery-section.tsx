0fd34cd3b2a6251a85b8c5ea506a6ce5aa0f815d
natuwa <71311324+natuwa@users.noreply.github.com>
2026-04-12 17:58:31 +0530

    Update gallery-section.tsx
    onclick

--- components/gallery-section.tsx
+++ components/gallery-section.tsx
@@ -37,7 +37,7 @@ export function GallerySection() {
             <div
               key={index}
               className="relative cursor-pointer overflow-hidden rounded-xl group"
-              nClick={() => setSelectedImage(image.src)}
+              onClick={() => setSelectedImage(image.src)}
             >
               <div className="relative aspect-square">
                 <Image
