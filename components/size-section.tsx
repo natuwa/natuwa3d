8165b46315dfd082b2e1b21aa1b7df34dfc145c8
natuwa <71311324+natuwa@users.noreply.github.com>
2026-04-13 22:11:29 +0530

    Update size-section.tsx
    size guide

--- components/size-section.tsx
+++ components/size-section.tsx
@@ -6,16 +6,19 @@ export default function SizeSection() {
       </h2>
 
       <p className="text-gray-600 mb-10">
-        Available in 4, 6, 8 & 10 inches — crafted to match your space and style
+        Available in 4, 6, 8 , 10 & 12 inches — crafted to match your space and style
       </p>
 
       <div className="flex justify-center">
         <img
-          src="/images/size-guide.jpg"
+          src="/images/size.jpg"
           alt="Miniature Size Guide"
           className="rounded-xl shadow-md max-w-4xl w-full transition-transform duration-300 hover:scale-105"
         />
       </div>
+      <p className="mt-6 text-sm text-gray-500 text-center">
+        All sizes maintain same quality & detailing — only the scale changes.
+      </p>
 
       <div className="grid grid-cols-2 md:grid-cols-4 gap-6 mt-10 text-sm">
         <div>
