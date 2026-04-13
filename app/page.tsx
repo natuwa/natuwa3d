47e5d2d37f3d676e19807c8c9030612f77c694f3
natuwa <71311324+natuwa@users.noreply.github.com>
2026-04-13 22:05:11 +0530

    Update page.tsx
    size section added

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import { Navbar } from "@/components/navbar"
 import { HeroSection } from "@/components/hero-section"
 import { ProductsSection } from "@/components/products-section"
+import { SizeSection } from "@/components/size-section";
 import { ProcessSection } from "@/components/process-section"
 import { GallerySection } from "@/components/gallery-section"
 import { TestimonialsSection } from "@/components/testimonials-section"
 import { ContactSection } from "@/components/contact-section"
 import { Footer } from "@/components/footer"
+
 
 export default function Home() {
   return (
@@ -13,6 +15,7 @@ export default function Home() {
       <Navbar />
       <HeroSection />
       <ProductsSection />
+      <SizeSection />
       <ProcessSection />
       <GallerySection />
       <TestimonialsSection />
