d72d6c3a8b725de08ac516b061046a840bd12d32
natuwa <71311324+natuwa@users.noreply.github.com>
2026-04-13 22:43:13 +0530

    Update page.tsx
    zxcvbn

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import { Navbar } from "@/components/navbar"
 import { HeroSection } from "@/components/hero-section"
 import { ProductsSection } from "@/components/products-section"
-import { SizeSection } from "@/components/size-section";
+import  SizeSection  from "@/components/size-section";
 import { ProcessSection } from "@/components/process-section"
 import { GallerySection } from "@/components/gallery-section"
 import { TestimonialsSection } from "@/components/testimonials-section"
