3d2fdad30954bc5009da8abd3f90292c2ace7b5e
natuwa <71311324+natuwa@users.noreply.github.com>
2026-05-16 22:05:15 +0530

    Create page.tsx
    meta blog

---
+++ app/blog/best-wedding-gift-ideas/page.tsx
@@ -0,0 +1,14 @@
+export const metadata = {
+  title: "Best Wedding Gift Ideas in India | Natuwa3D",
+  
+  description:
+    "Discover personalized wedding gift ideas including custom 3D printed bride groom miniatures in India.",
+
+  keywords: [
+    "Wedding Gift Ideas",
+    "Custom Miniature",
+    "Bride Groom Miniature",
+    "3D Printed Miniatures",
+    "Personalized Gifts India",
+  ],
+};
