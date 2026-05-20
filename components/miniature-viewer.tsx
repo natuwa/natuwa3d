e0b4ae7739f4d10d85d7a7efac4524f85ec48056
natuwa <71311324+natuwa@users.noreply.github.com>
2026-05-20 21:01:37 +0530

    Update miniature-viewer.tsx
    models added

--- components/miniature-viewer.tsx
+++ components/miniature-viewer.tsx
@@ -1,30 +1,35 @@
 "use client";
 
 import { Canvas } from "@react-three/fiber";
-import { OrbitControls } from "@react-three/drei";
+import { OrbitControls, useGLTF } from "@react-three/drei";
 
-function Box() {
+function Model() {
+  const model = useGLTF("/models/miniature.glb");
+
   return (
-    <mesh>
-      <boxGeometry args={[2, 2, 2]} />
-      <meshStandardMaterial color="#b08968" />
-    </mesh>
+    <primitive
+      object={model.scene}
+      scale={2}
+    />
   );
 }
 
 export default function MiniatureViewer() {
   return (
-    <div className="w-full h-[400px] rounded-3xl overflow-hidden bg-[#f8f5f2]">
+    <div className="w-full h-[500px] rounded-3xl overflow-hidden bg-[#f8f5f2]">
 
       <Canvas camera={{ position: [0, 0, 5] }}>
 
         <ambientLight intensity={1.5} />
 
-        <directionalLight position={[2, 2, 2]} />
+        <directionalLight position={[2, 2, 2]} intensity={2} />
 
-        <Box />
+        <Model />
 
-        <OrbitControls />
+        <OrbitControls
+          enableZoom={true}
+          autoRotate={true}
+        />
 
       </Canvas>
 
