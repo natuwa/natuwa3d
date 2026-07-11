a68c944e905dfb52cc9782b34c13c9662024d96c
natuwa <71311324+natuwa@users.noreply.github.com>
2026-07-11 13:39:54 +0530

    Update page.tsx
    page updated

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,13 +1,41 @@
+import DashboardCard from "./components/DashboardCard";
+
 export default function AdminPage() {
   return (
-    <>
-      <h1 className="text-3xl font-bold">
+    <div>
+
+      <h1 className="text-3xl font-bold mb-8">
         Dashboard
       </h1>
 
-      <p className="text-gray-500 mt-2">
-        Welcome to NATUWA3D Admin Panel.
-      </p>
-    </>
+      <div className="grid grid-cols-4 gap-6">
+
+        <DashboardCard
+          title="Today's Inquiry"
+          value="5"
+          color="bg-blue-600"
+        />
+
+        <DashboardCard
+          title="Revenue"
+          value="₹12,500"
+          color="bg-green-600"
+        />
+
+        <DashboardCard
+          title="Active Orders"
+          value="14"
+          color="bg-purple-600"
+        />
+
+        <DashboardCard
+          title="Packed"
+          value="3"
+          color="bg-orange-500"
+        />
+
+      </div>
+
+    </div>
   );
 }
