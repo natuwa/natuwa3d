ca1f7f9008ae50171fb5b1ae216c139c7aa66b91
natuwa <71311324+natuwa@users.noreply.github.com>
2026-07-11 13:30:04 +0530

    Create layout.tsx
    layout addeddd

---
+++ app/admin/layout.tsx
@@ -0,0 +1,22 @@
+import Sidebar from "./components/Sidebar";
+import Header from "./components/Header";
+
+export default function AdminLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="flex min-h-screen bg-gray-100">
+      <Sidebar />
+
+      <div className="flex-1 flex flex-col">
+        <Header />
+
+        <main className="p-8">
+          {children}
+        </main>
+      </div>
+    </div>
+  );
+}
