cf4ca8a4b886ba120660476ecdb2b52ff3c59d65
natuwa <71311324+natuwa@users.noreply.github.com>
2026-07-11 13:28:02 +0530

    Create Header.tsx
    header added

---
+++ app/admin/components/Header.tsx
@@ -0,0 +1,13 @@
+export default function Header() {
+  return (
+    <header className="bg-white border-b px-8 py-5 flex justify-between items-center">
+      <h2 className="text-2xl font-semibold">
+        Dashboard
+      </h2>
+
+      <div className="font-medium">
+        Welcome, Admin 👋
+      </div>
+    </header>
+  );
+}
