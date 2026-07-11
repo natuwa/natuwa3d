d1874398795f63bc04c31360529dccf2d15024b1
natuwa <71311324+natuwa@users.noreply.github.com>
2026-07-11 13:18:56 +0530

    Create          page.tsx
    admin page added

---
+++ app/admin/page.tsx
@@ -0,0 +1,9 @@
+export default function AdminPage() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold">
+        NATUWA3D ADMIN PANEL
+      </h1>
+    </div>
+  );
+}
