67210d6ef6946de02174528d01f48cf52fb154dc
natuwa <71311324+natuwa@users.noreply.github.com>
2026-07-11 15:34:37 +0530

    Update layout.tsx
    google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -217,6 +217,21 @@ export default function RootLayout({
             }),
           }}
         />
+{/* Google Analytics */}
+<Script
+  src="https://www.googletagmanager.com/gtag/js?id=G-7WMS821YBD"
+  strategy="afterInteractive"
+/>
+
+<Script id="google-analytics" strategy="afterInteractive">
+  {`
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-7WMS821YBD');
+  `}
+</Script>
         {children}
         <script
           type="module"
