47a37cc50ffbc0dde8155ad4b7ac9baf690db3b0
natuwa <71311324+natuwa@users.noreply.github.com>
2026-07-11 15:57:21 +0530

    Update page.tsx
    code update admin page

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,37 +1,60 @@
-import DashboardCard from "./components/DashboardCard";
+import StatsCard from "./components/StatsCard";
+
+import {
+  ShoppingBag,
+  IndianRupee,
+  Package,
+  Clock3,
+} from "lucide-react";
 
 export default function AdminPage() {
   return (
     <div>
 
-      <h1 className="text-3xl font-bold mb-8">
-        Dashboard
-      </h1>
+      {/* Heading */}
+      <div className="mb-8">
+        <h1 className="text-3xl font-bold text-gray-900">
+          Dashboard
+        </h1>
 
-      <div className="grid grid-cols-4 gap-6">
+        <p className="text-gray-500 mt-2">
+          Welcome back 👋 Here's today's business overview.
+        </p>
+      </div>
 
-        <DashboardCard
+      {/* Stats Cards */}
+      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-6">
+
+        <StatsCard
           title="Today's Inquiry"
-          value="5"
+          value={5}
+          subtitle="2 more than yesterday"
           color="bg-blue-600"
+          icon={<ShoppingBag size={28} />}
         />
 
-        <DashboardCard
+        <StatsCard
           title="Revenue"
           value="₹12,500"
+          subtitle="Today's Collection"
           color="bg-green-600"
+          icon={<IndianRupee size={28} />}
         />
 
-        <DashboardCard
+        <StatsCard
           title="Active Orders"
-          value="14"
+          value={14}
+          subtitle="Currently in Production"
           color="bg-purple-600"
+          icon={<Package size={28} />}
         />
 
-        <DashboardCard
-          title="Packed"
-          value="3"
+        <StatsCard
+          title="Pending Follow-up"
+          value={3}
+          subtitle="Need Your Attention"
           color="bg-orange-500"
+          icon={<Clock3 size={28} />}
         />
 
       </div>
