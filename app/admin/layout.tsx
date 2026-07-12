c82f954e6280d2cbaf33d42338e301deffcd790c
natuwa <71311324+natuwa@users.noreply.github.com>
2026-07-12 23:36:19 +0530

    Update layout.tsx
    mnvbcbvghgfvmjhg

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,33 +1,22 @@
 import Sidebar from "./components/Sidebar";
 import Header from "./components/Header";
-import AdminGuard from "./components/AdminGuard";
-import { headers } from "next/headers";
 
-export default async function AdminLayout({
+export default function AdminLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const pathname =
-    (await headers()).get("x-invoke-path") || "";
-
-  if (pathname.includes("/admin/login")) {
-    return <>{children}</>;
-  }
-
   return (
-    <AdminGuard>
-      <div className="flex min-h-screen bg-gray-100">
-        <Sidebar />
+    <div className="flex min-h-screen bg-gray-100">
+      <Sidebar />
 
-        <div className="flex-1 flex flex-col">
-          <Header />
+      <div className="flex-1 flex flex-col">
+        <Header />
 
-          <main className="p-8">
-            {children}
-          </main>
-        </div>
+        <main className="p-8">
+          {children}
+        </main>
       </div>
-    </AdminGuard>
+    </div>
   );
 }
