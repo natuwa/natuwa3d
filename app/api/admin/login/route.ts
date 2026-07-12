261abf2ea6ba794cf3be24bfa3bfae524e8f2986
natuwa <71311324+natuwa@users.noreply.github.com>
2026-07-13 00:22:32 +0530

    Update route.ts
    bvbvnbhvnvb

--- app/api/admin/login/route.ts
+++ app/api/admin/login/route.ts
@@ -1,10 +1,42 @@
-const ADMIN_USERNAME = "admin";
-const ADMIN_PASSWORD = "Natuwa@123";
+import { NextResponse } from "next/server";
 
-console.log("Username:", username);
-console.log("Password:", password);
+export async function POST(req: Request) {
+  const { username, password } = await req.json();
 
-console.log(
-  username === ADMIN_USERNAME,
-  password === ADMIN_PASSWORD
-);
+  const ADMIN_USERNAME = "admin";
+  const ADMIN_PASSWORD = "Natuwa@123";
+
+  // Debug Logs
+  console.log("Username Entered:", username);
+  console.log("Password Entered:", password);
+
+  console.log("Username Match:", username === ADMIN_USERNAME);
+  console.log("Password Match:", password === ADMIN_PASSWORD);
+
+  if (
+    username === ADMIN_USERNAME &&
+    password === ADMIN_PASSWORD
+  ) {
+    const response = NextResponse.json({
+      success: true,
+    });
+
+    response.cookies.set("admin-auth", "loggedin", {
+      httpOnly: true,
+      secure: true,
+      sameSite: "lax",
+      path: "/",
+      maxAge: 60 * 60 * 24 * 7,
+    });
+
+    return response;
+  }
+
+  return NextResponse.json(
+    {
+      success: false,
+      message: "Invalid Username or Password",
+    },
+    { status: 401 }
+  );
+}
