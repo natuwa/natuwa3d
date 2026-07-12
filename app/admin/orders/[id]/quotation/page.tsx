51e320d2cf700b00410a311eada39b9287380452
natuwa <71311324+natuwa@users.noreply.github.com>
2026-07-12 18:31:54 +0530

    Update page.tsx
    kjhgdfgnnbvcd

--- app/admin/orders/[id]/quotation/page.tsx
+++ app/admin/orders/[id]/quotation/page.tsx
@@ -33,12 +33,14 @@ export default async function QuotationPage({
 
         <div className="mb-6 flex justify-between">
 
-          <button
-            onClick={() => window.print()}
-            className="bg-blue-600 hover:bg-blue-700 text-white px-5 py-3 rounded-lg"
-          >
-            🖨 Print / Save PDF
-          </button>
+          {/* 
+<button
+  onClick={() => window.print()}
+  className="bg-blue-600 hover:bg-blue-700 text-white px-5 py-3 rounded-lg"
+>
+  🖨 Print / Save PDF
+</button>
+*/}
 
         </div>
 
