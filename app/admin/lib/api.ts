d35bd4ed2d000c99032fa4e88c42e0716320975c
natuwa <71311324+natuwa@users.noreply.github.com>
2026-08-26 01:08:43 +0530

    Update api.ts
    vzxvm,mnb

--- app/admin/lib/api.ts
+++ app/admin/lib/api.ts
@@ -1,5 +1,5 @@
 const API_URL =
-  "https://script.google.com/macros/s/AKfycbxTZs29V6KUcXMpd0wBtw0R9bWorm-VZS4BM8V4QXlWBMdBubRmckHLMo0oLA35zoRJ/exec?action=dashboard";
+  "https://script.google.com/macros/s/AKfycby7HnYojREucqhi60SYEpvXnrYR7XGnzrkY0SEloPKgXquVDqquqqtIXpsSawRj8YD0/exec?action=dashboard";
 
 export async function getOrders() {
   const res = await fetch(API_URL, {
