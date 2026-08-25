77228ac577b0808aee260c062f34195b2cfce4cf
natuwa <71311324+natuwa@users.noreply.github.com>
2026-08-26 01:05:26 +0530

    Update api.ts
    ,fcgnmbd

--- app/admin/lib/api.ts
+++ app/admin/lib/api.ts
@@ -1,5 +1,5 @@
 const API_URL =
-  "https://script.google.com/macros/s/AKfycbwF54tHs0Sz4sUK67hJu_7jEXp8NqYZo2X2VsbspLut6Ybo4MT_fw_g9WyAtrOWDPSn/exec?action=dashboard";
+  "https://script.google.com/macros/s/AKfycbxTZs29V6KUcXMpd0wBtw0R9bWorm-VZS4BM8V4QXlWBMdBubRmckHLMo0oLA35zoRJ/exec?action=dashboard";
 
 export async function getOrders() {
   const res = await fetch(API_URL, {
